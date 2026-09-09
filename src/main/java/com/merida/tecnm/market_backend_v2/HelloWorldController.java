8f870a431a4b9ac41397fb9dd41f299799716c9b
aaroncarv777 <carvajalaaron44@gmail.com>
2026-09-08 18:26:10 -0600

    Adding HelloWorldController to test Spring Configuration

--- src/main/java/com/merida/tecnm/market_backend_v2/HelloWorldController.java
+++ src/main/java/com/merida/tecnm/market_backend_v2/HelloWorldController.java
@@ -4,9 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class HelloWorldController {
-     @GetMapping("/saludo")
-     public String helloworld() {
-     return "Hello World!";
-     }
+
+    @GetMapping("/saludo")
+    public String helloworld() {
+        return "Hello World!";
+    }
 }
